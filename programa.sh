9cd3e9ee0d480818405642e31070ce4dea1a2073
root <root@jessie.raw>
2018-02-13 11:17:03 +0000

    bash

---
+++ programa.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+read -p "Nome: " Nome
+if [ "$Nome" == "$(cat informaçoes.txt | grep ^$Nome | cut -d ':' -f 1 )" ]; then
+read -p "Email: " Email
+	if [ "$Email" == "$( cat informaçoes.txt | grep $Email | cut -d ':' -f 2)" ]; then 
+	read -p "Telefone: " Tel
+		if [ "$Tel" == "$( cat informaçoes.txt | grep -E '[(][0-9]{2}[)][0-9]{4}+-[0-9]{4}' | cut -d ':' -f3)" ]; then  
+		read -p "RG: " RG 
+			if [ "$RG" == "$( cat informaçoes.txt | grep -E '[0-9]{2}[.][0-9]{3}[.][0-9]{3}[-][0-9]{1}' | cut -d ':' -f4)" ]; then
+			read -p "CPF: " CPF
+				if [ "$CPF" == "$( cat informaçoes.txt | grep -E '[0-9]{3}[.][0-9]{3}[.][0-9]{3}[-][0-9]{2}' | cut -d ':' -f5)" ]; then
+				read -p "Data De Nascimento: " Data
+					if [ "$Data" == "$( cat informaçoes.txt | grep -E '[0-9]{2}[/][0-9]{2}[/][0-9]{4}' | cut -d ':' -f6)" ]; then
+					read -p "IP: " IP
+						if [ "$IP" == "$( cat informaçoes.txt | grep -E '[0-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3}' | cut -d ':' -f7)" ]; then
+						read -p "MASCARA: " MASC
+							if [ "$MASC" == "$( cat informaçoes.txt | grep -E '[0-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3}' | cut -d ':' -f8)" ]; then
+							echo "Dados 100% corretos"
+							else
+							echo "Mascara incorreta"
+							exit
+							fi
+						else
+						echo "IP incorreto"
+						exit
+						fi
+					else
+					echo "Data de Nascimento errada"
+					exit
+					fi
+				else
+				echo "CPF incorreto"
+				exit
+				fi
+			else
+			echo "RG incorreto"
+			exit
+			fi
+		else
+		echo "telefone incorreto" 
+		exit
+		fi
+	else
+	echo "email incorreto"
+	exit
+	fi
+else
+	echo "nome incorreto"
+	exit
+fi
